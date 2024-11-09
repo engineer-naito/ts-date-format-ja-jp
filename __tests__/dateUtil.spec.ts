e5212e20f8a06e36bb9835d893471d0016c1d9bc
Arahabica <r.sasage@gmail.com>
2024-11-10 01:03:16 +0900

    init

---
+++ __tests__/dateUtil.spec.ts
@@ -0,0 +1,259 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { formatDate } from '../src/dateUtil';
+
+describe('formatDate', () => {
+  let preTimezone: string | undefined;
+  beforeAll(() => {
+    preTimezone = process.env.TZ;
+    // タイムゾーンを固定
+    process.env.TZ = 'Asia/Tokyo';
+  });
+  afterAll(() => {
+    process.env.TZ = preTimezone;
+  });
+  const testDate = new Date('2024-04-09T00:01:02.123Z'); // 固定されたテスト日付 (4月9日)
+  const pm = new Date('2024-04-09T12:00:00.123Z'); // 午後9時
+  const heiseiLast = new Date('2019-04-30T14:59:59.999Z'); // 令和元年の1日前 (2019月4月30日)
+  const reiwaFirst = new Date('2019-04-30T15:00:00.000Z'); // 令和元年 (2019月5月1日)
+  const sekigahara = new Date('1600-10-21T01:00:00.000Z'); // 慶長5年10月21日10時
+
+  it('formats date as YYYY-MM-DD', () => {
+    expect(formatDate(testDate, 'YYYY-MM-DD')).toBe('2024-04-09');
+  });
+
+  it('formats date as YYYY/MM/DD', () => {
+    expect(formatDate(testDate, 'YYYY/MM/DD')).toBe('2024/04/09');
+  });
+
+  it('formats date as YYYY/MM/DD(ddd)', () => {
+    expect(formatDate(testDate, 'YYYY/MM/DD(ddd)')).toBe('2024/04/09(Tue)');
+  });
+
+  it('formats date as YYYY/MM/DD(曜)', () => {
+    expect(formatDate(testDate, 'YYYY/MM/DD(曜)')).toBe('2024/04/09(火)');
+  });
+
+  it('formats date as YYYY年M月D日', () => {
+    expect(formatDate(testDate, 'YYYY年M月D日')).toBe('2024年4月9日');
+  });
+
+  it('formats date as YYYY年M月D日(曜)', () => {
+    expect(formatDate(testDate, 'YYYY年M月D日(曜)')).toBe('2024年4月9日(火)');
+  });
+
+  it('formats date as (元号)Y年M月D日', () => {
+    expect(formatDate(testDate, '(元号)Y年M月D日')).toBe('令和6年4月9日');
+    expect(formatDate(heiseiLast, '(元号)Y年M月D日')).toBe('平成31年4月30日');
+    expect(formatDate(reiwaFirst, '(元号)Y年M月D日')).toBe('令和元年5月1日');
+    expect(formatDate(sekigahara, '(元号)Y年M月D日')).toBe('慶長5年10月21日'); // 注) 月日は現行の太陽暦。
+  });
+
+  it('formats date as (元号)Y年M月D日(曜)', () => {
+    expect(formatDate(testDate, '(元号)Y年M月D日(曜)')).toBe('令和6年4月9日(火)');
+  });
+
+  it('formats date as (GENGOU)Y.M.D', () => {
+    expect(formatDate(testDate, '(GENGOU)Y.M.D')).toBe('R6.4.9');
+    expect(formatDate(heiseiLast, '(GENGOU)Y.M.D')).toBe('H31.4.30');
+    expect(formatDate(reiwaFirst, '(GENGOU)Y.M.D')).toBe('R1.5.1');
+    expect(formatDate(new Date(1980, 0, 1), '(GENGOU)Y.M.D')).toBe('S55.1.1'); // 昭和55年1月1日
+    expect(formatDate(new Date(1920, 0, 1), '(GENGOU)Y.M.D')).toBe('T9.1.1'); // 大正9年1月1日
+    expect(formatDate(new Date(1880, 0, 1), '(GENGOU)Y.M.D')).toBe('M13.1.1'); // 明治13年1月1日
+    expect(formatDate(new Date(1868, 0, 1), '(GENGOU)Y.M.D')).toBe('慶応4.1.1'); // 慶応4年1月1日
+  });
+
+  it('formats date as (GENGOU)Y.MM.DD', () => {
+    expect(formatDate(testDate, '(GENGOU)Y.MM.DD')).toBe('R6.04.09');
+    expect(formatDate(heiseiLast, '(GENGOU)Y.MM.DD')).toBe('H31.04.30');
+    expect(formatDate(reiwaFirst, '(GENGOU)Y.MM.DD')).toBe('R1.05.01');
+  });
+
+  it('formats date as MM/DD', () => {
+    expect(formatDate(testDate, 'MM/DD')).toBe('04/09');
+  });
+
+  it('formats date as MM/DD(ddd)', () => {
+    expect(formatDate(testDate, 'MM/DD(ddd)')).toBe('04/09(Tue)');
+  });
+
+  it('formats date as MM/DD(曜)', () => {
+    expect(formatDate(testDate, 'MM/DD(曜)')).toBe('04/09(火)');
+  });
+
+  it('formats date as M/D', () => {
+    expect(formatDate(testDate, 'M/D')).toBe('4/9');
+  });
+
+  it('formats date as M/D(ddd)', () => {
+    expect(formatDate(testDate, 'M/D(ddd)')).toBe('4/9(Tue)');
+  });
+
+  it('formats date as M/D(曜)', () => {
+    expect(formatDate(testDate, 'M/D(曜)')).toBe('4/9(火)');
+  });
+
+  it('formats date as M月D日', () => {
+    expect(formatDate(testDate, 'M月D日')).toBe('4月9日');
+  });
+
+  it('formats date as M月D日(曜)', () => {
+    expect(formatDate(testDate, 'M月D日(曜)')).toBe('4月9日(火)');
+  });
+
+  it('formats date as HH:mm:ss.SSS', () => {
+    expect(formatDate(testDate, 'HH:mm:ss.SSS')).toBe('09:01:02.123');
+    expect(formatDate(pm, 'HH:mm:ss.SSS')).toBe('21:00:00.123');
+  });
+
+  it('formats date as HH:mm:ss', () => {
+    expect(formatDate(testDate, 'HH:mm:ss')).toBe('09:01:02');
+    expect(formatDate(pm, 'HH:mm:ss')).toBe('21:00:00');
+  });
+
+  it('formats date as HH:mm', () => {
+    expect(formatDate(testDate, 'HH:mm')).toBe('09:01');
+    expect(formatDate(pm, 'HH:mm')).toBe('21:00');
+  });
+
+  it('formats date as HH時mm分ss秒', () => {
+    expect(formatDate(testDate, 'HH時mm分ss秒')).toBe('09時01分02秒');
+    expect(formatDate(pm, 'HH時mm分ss秒')).toBe('21時00分00秒');
+  });
+
+  it('formats date as HH時mm分', () => {
+    expect(formatDate(testDate, 'HH時mm分')).toBe('09時01分');
+    expect(formatDate(pm, 'HH時mm分')).toBe('21時00分');
+  });
+
+  it('formats date as H時m分', () => {
+    expect(formatDate(testDate, 'H時m分')).toBe('9時1分');
+    expect(formatDate(pm, 'H時m分')).toBe('21時0分');
+  });
+
+  it('formats date as HH時', () => {
+    expect(formatDate(testDate, 'HH時')).toBe('09時');
+    expect(formatDate(pm, 'HH時')).toBe('21時');
+  });
+
+  it('formats date as H時', () => {
+    expect(formatDate(testDate, 'H時')).toBe('9時');
+    expect(formatDate(pm, 'H時')).toBe('21時');
+  });
+
+  it('formats date as ISO8601', () => {
+    expect(formatDate(testDate, 'ISO8601')).toBe('2024-04-09T00:01:02.123Z');
+  });
+
+  it('formats date as YYYY-MM-DDTHH:mm:ss.SSSZ', () => {
+    expect(formatDate(testDate, 'YYYY-MM-DDTHH:mm:ss.SSSZ')).toBe('2024-04-09T00:01:02.123Z');
+  });
+
+  it('formats date as YYYY-MM-DD HH:mm:ss', () => {
+    expect(formatDate(testDate, 'YYYY-MM-DD HH:mm:ss')).toBe('2024-04-09 09:01:02');
+  });
+
+  it('formats date as YYYY/MM/DD HH:mm:ss', () => {
+    expect(formatDate(testDate, 'YYYY/MM/DD HH:mm:ss')).toBe('2024/04/09 09:01:02');
+  });
+
+  it('formats date as YYYY/MM/DD HH:mm', () => {
+    expect(formatDate(testDate, 'YYYY/MM/DD HH:mm')).toBe('2024/04/09 09:01');
+  });
+
+  it('formats date as YYYY年MM月DD日HH時mm分ss秒', () => {
+    expect(formatDate(testDate, 'YYYY年MM月DD日HH時mm分ss秒')).toBe('2024年04月09日09時01分02秒');
+  });
+
+  it('formats date as YYYY年M月D日H時m分', () => {
+    expect(formatDate(testDate, 'YYYY年M月D日H時m分')).toBe('2024年4月9日9時1分');
+    expect(formatDate(pm, 'YYYY年M月D日H時m分')).toBe('2024年4月9日21時0分');
+  });
+
+  it('formats date as YYYY年M月D日H時', () => {
+    expect(formatDate(testDate, 'YYYY年M月D日H時')).toBe('2024年4月9日9時');
+    expect(formatDate(pm, 'YYYY年M月D日H時')).toBe('2024年4月9日21時');
+  });
+
+  it('formats date as (元号)Y年M月D日H時m分', () => {
+    expect(formatDate(testDate, '(元号)Y年M月D日H時m分')).toBe('令和6年4月9日9時1分');
+    expect(formatDate(heiseiLast, '(元号)Y年M月D日H時m分')).toBe('平成31年4月30日23時59分');
+  });
+
+  it('formats date as (元号)Y年M月D日H時', () => {
+    expect(formatDate(testDate, '(元号)Y年M月D日H時')).toBe('令和6年4月9日9時');
+    expect(formatDate(heiseiLast, '(元号)Y年M月D日H時')).toBe('平成31年4月30日23時');
+  });
+
+  it('formats date as MM/DD HH:mm:ss', () => {
+    expect(formatDate(testDate, 'MM/DD HH:mm:ss')).toBe('04/09 09:01:02');
+  });
+  
+  it('formats date as MM/DD HH:mm', () => {
+    expect(formatDate(testDate, 'MM/DD HH:mm')).toBe('04/09 09:01');
+  });
+
+  it('formats date as MM月DD日HH時mm分ss秒', () => {
+    expect(formatDate(testDate, 'MM月DD日HH時mm分ss秒')).toBe('04月09日09時01分02秒');
+  });
+
+  it('formats date as M月D日H時m分', () => {
+    expect(formatDate(testDate, 'M月D日H時m分')).toBe('4月9日9時1分');
+    expect(formatDate(pm, 'M月D日H時m分')).toBe('4月9日21時0分');
+  });
+
+  it('formats date as M月D日H時', () => {
+    expect(formatDate(testDate, 'M月D日H時')).toBe('4月9日9時');
+    expect(formatDate(pm, 'M月D日H時')).toBe('4月9日21時');
+  });
+
+  it('formats date as YYYY', () => {
+    expect(formatDate(testDate, 'YYYY')).toBe('2024');
+  });
+
+  it('formats date as MM', () => {
+    expect(formatDate(testDate, 'MM')).toBe('04');
+  });
+
+  it('formats date as M', () => {
+    expect(formatDate(testDate, 'M')).toBe('4');
+  });
+
+  it('formats date as DD', () => {
+    expect(formatDate(testDate, 'DD')).toBe('09');
+  });
+
+  it('formats date as D', () => {
+    expect(formatDate(testDate, 'D')).toBe('9');
+  });
+
+  it('formats time as HH', () => {
+    expect(formatDate(testDate, 'HH')).toBe('09');
+  });
+
+  it('formats time as H', () => {
+    expect(formatDate(testDate, 'H')).toBe('9');
+  });
+
+  it('formats time as mm', () => {
+    expect(formatDate(testDate, 'mm')).toBe('01');
+  });
+
+  it('formats time as m', () => {
+    expect(formatDate(testDate, 'm')).toBe('1');
+  });
+
+  it('formats time as ss', () => {
+    expect(formatDate(testDate, 'ss')).toBe('02');
+  });
+
+  it('formats time as s', () => {
+    expect(formatDate(testDate, 's')).toBe('2');
+  });
+
+  it('Unsupported format throws error', () => {
+    // @ts-ignore
+    expect(() => formatDate(testDate, 'YYYY-MM-DD mm:ss')).toThrow(
+      'Unsupported format: YYYY-MM-DD mm:ss'
+    );
+  });
+});
---
+++ src/dateUtil.ts
@@ -0,0 +1,772 @@
+type SupportedDateFormat = 
+  'YYYY-MM-DD' |
+  'YYYY/MM/DD' |
+  'YYYY/MM/DD(ddd)' |
+  'YYYY/MM/DD(曜)' |
+  'YYYY年M月D日'|
+  'YYYY年M月D日(曜)'|
+  '(元号)Y年M月D日' |
+  '(元号)Y年M月D日(曜)' |
+  '(GENGOU)Y.M.D' |
+  '(GENGOU)Y.MM.DD' |
+  'MM/DD' |
+  'MM/DD(ddd)' |
+  'MM/DD(曜)' |
+  'M/D' |
+  'M/D(ddd)' |
+  'M/D(曜)' |
+  'M月D日'|
+  'M月D日(曜)'|
+  'HH:mm:ss.SSS' |
+  'HH:mm:ss' |
+  'HH:mm' |
+  'HH時mm分ss秒'|
+  'HH時mm分'|
+  'H時m分'|
+  'HH時'|
+  'H時'|
+  'ISO8601' |
+  'YYYY-MM-DDTHH:mm:ss.SSSZ' |
+  'YYYY-MM-DD HH:mm:ss' |
+  'YYYY/MM/DD HH:mm:ss' |
+  'YYYY/MM/DD HH:mm' |
+  'YYYY年MM月DD日HH時mm分ss秒'|
+  'YYYY年M月D日H時m分'|
+  'YYYY年M月D日H時'|
+  '(元号)Y年M月D日H時m分'|
+  '(元号)Y年M月D日H時'|
+  'MM/DD HH:mm:ss' |
+  'MM/DD HH:mm' |
+  'MM月DD日HH時mm分ss秒'|
+  'M月D日H時m分'|
+  'M月D日H時'|
+  'YYYY' |
+  'MM' |
+  'M' |
+  'DD' |
+  'D' |
+  'HH' |
+  'H' |
+  'mm' |
+  'm' |
+  'ss' |
+  's';
+
+/**
+ * 日付を YYYY-MM-DD 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format YYYY-MM-DD
+ * @returns {string} 例: 2024-04-08
+ */
+export function formatDate(date: Date, format: 'YYYY-MM-DD'): string;
+
+/**
+ * 日付を YYYY/MM/DD 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format YYYY/MM/DD
+ * @returns {string} 例: 2024/04/08
+ */
+export function formatDate(date: Date, format: 'YYYY/MM/DD'): string;
+
+/**
+ * 日付を YYYY/MM/DD(ddd) 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format YYYY/MM/DD(ddd)
+ * @returns {string} 例: 2024/04/08(Mon)
+ */
+export function formatDate(date: Date, format: 'YYYY/MM/DD(ddd)'): string;
+
+/**
+ * 日付を YYYY/MM/DD(曜) 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format YYYY/MM/DD(曜)
+ * @returns {string} 例: 2024/04/08(月)
+ */
+export function formatDate(date: Date, format: 'YYYY/MM/DD(曜)'): string;
+
+/**
+ * 日付を YYYY年M月D日 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format YYYY年M月D日
+ * @returns {string} 例: 2024年4月8日
+ */
+export function formatDate(date: Date, format: 'YYYY年M月D日'): string;
+
+/**
+ * 日付を YYYY年M月D日(曜) 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format YYYY年M月D日(曜)
+ * @returns {string} 例: 2024年4月8日(月)
+ */
+export function formatDate(date: Date, format: 'YYYY年M月D日(曜)'): string;
+
+/**
+ * 日付を (元号)Y年M月D日 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format (元号)Y年M月D日
+ * @returns {string} 例: 令和6年4月8日
+ */
+export function formatDate(date: Date, format: '(元号)Y年M月D日'): string;
+
+/**
+ * 日付を (元号)Y年M月D日(曜) 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format (元号)Y年M月D日(曜)
+ * @returns {string} 例: 令和6年4月8日(月)
+ */
+export function formatDate(date: Date, format: '(元号)Y年M月D日(曜)'): string;
+
+/**
+ * 日付を (GENGOU)Y.M.D 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format (GENGOU)Y.M.D
+ * @returns {string} 例: R6.4.8
+ */
+export function formatDate(date: Date, format: '(GENGOU)Y.M.D'): string;
+
+/**
+ * 日付を (GENGOU)Y.MM.DD 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format (GENGOU)Y.MM.DD
+ * @returns {string} 例: R06.04.08
+ */
+export function formatDate(date: Date, format: '(GENGOU)Y.MM.DD'): string;
+
+/**
+ * 日付を MM/DD 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format MM/DD
+ * @returns {string} 例: 04/08
+ */
+export function formatDate(date: Date, format: 'MM/DD'): string;
+
+
+/**
+ * 日付を MM/DD(ddd) 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format MM/DD(ddd)
+ * @returns {string} 例: 04/08(Mon)
+ */
+export function formatDate(date: Date, format: 'MM/DD(ddd)'): string;
+
+/**
+ * 日付を MM/DD 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format MM/DD(曜)
+ * @returns {string} 例: 04/08(月)
+ */
+export function formatDate(date: Date, format: 'MM/DD(曜)'): string;
+
+/**
+ * 日付を M/D 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format M/D
+ * @returns {string} 例: 4/8
+ */
+export function formatDate(date: Date, format: 'M/D'): string;
+
+/**
+ * 日付を M/D(ddd) 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format M/D(ddd)
+ * @returns {string} 例: 04/08(Mon)
+ */
+export function formatDate(date: Date, format: 'M/D(ddd)'): string;
+
+/**
+ * 日付を M/D 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format M/D(曜)
+ * @returns {string} 例: 4/8(月)
+ */
+export function formatDate(date: Date, format: 'M/D(曜)'): string;
+
+/**
+ * 日付を M月D日 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format M月D日
+ * @returns {string} 例: 4月8日
+ */
+export function formatDate(date: Date, format: 'M月D日'): string;
+
+/**
+ * 日付を M月D日 形式の文字列に変換します
+ * @param date 変換対象の日付
+ * @param format M月D日(曜)
+ * @returns {string} 例: 4月8日(月)
+ */
+export function formatDate(date: Date, format: 'M月D日(曜)'): string;
+
+/**
+ * 時刻を HH:mm:ss.SSS 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format HH:mm:ss.SSS
+ * @returns {string} 例: 09:05:45.123
+ */
+export function formatDate(date: Date, format: 'HH:mm:ss.SSS'): string;
+
+/**
+ * 時刻を HH:mm:ss 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format HH:mm:ss
+ * @returns {string} 例: 09:05:45
+ */
+export function formatDate(date: Date, format: 'HH:mm:ss'): string;
+
+/**
+ * 時刻を HH:mm 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format HH:mm
+ * @returns {string} 例: 09:05
+ */
+export function formatDate(date: Date, format: 'HH:mm'): string;
+
+/**
+ * 時刻を HH時mm分ss秒 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format HH時mm分ss秒
+ * @returns {string} 例: 09時05分05秒
+ */
+export function formatDate(date: Date, format: 'HH時mm分ss秒'): string;
+
+/**
+ * 時刻を HH時mm分 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format HH時mm分
+ * @returns {string} 例: 09時05分
+ */
+export function formatDate(date: Date, format: 'HH時mm分'): string;
+
+/**
+ * 時刻を H時m分 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format H時m分
+ * @returns {string} 例: 9時5分
+ */
+export function formatDate(date: Date, format: 'H時m分'): string;
+
+/**
+ * 時刻を HH時 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format HH時
+ * @returns {string} 例: 09時
+ */
+export function formatDate(date: Date, format: 'HH時'): string;
+
+/**
+ * 時刻を H時 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format H時
+ * @returns {string} 例: 9時
+ */
+export function formatDate(date: Date, format: 'H時'): string;
+
+/**
+ * 日時を YYYY-MM-DDTHH:mm:ss.SSSZ 形式の文字列に変換します（ISO 8601）
+ * @param date 変換対象の日時
+ * @param format ISO8601
+ * @returns {string} 例: 2024-04-08T05:30:45.123Z
+ */
+export function formatDate(date: Date, format: 'ISO8601'): string;
+
+/**
+ * 日時を YYYY-MM-DDTHH:mm:ss.SSSZ 形式の文字列に変換します（ISO 8601）
+ * @param date 変換対象の日時
+ * @param format YYYY-MM-DDTHH:mm:ss.SSSZ
+ * @returns {string} 例: 2024-04-08T05:30:45.123Z
+ */
+export function formatDate(date: Date, format: 'YYYY-MM-DDTHH:mm:ss.SSSZ'): string;
+
+/**
+ * 日時を YYYY-MM-DD HH:mm:ss 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY-MM-DD HH:mm:ss
+ */
+export function formatDate(date: Date, format: 'YYYY-MM-DD HH:mm:ss'): string;
+
+/**
+ * 日時を YYYY/MM/DD HH:mm:ss 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY/MM/DD HH:mm:ss
+ * @returns {string} 例: 2024/04/08 09:05:45
+ */
+export function formatDate(date: Date, format: 'YYYY/MM/DD HH:mm:ss'): string;
+
+/**
+ * 日時を YYYY/MM/DD HH:mm 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY/MM/DD HH:mm
+ * @returns {string} 例: 2024/04/08 09:05
+ */
+export function formatDate(date: Date, format: 'YYYY/MM/DD HH:mm'): string;
+
+
+/**
+ * 日時を YYYY年MM月DD日HH時mm分ss秒 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY年MM月DD日HH時mm分ss秒
+ * @returns {string} 例: 2024年04月08日09時09分08秒
+ */
+export function formatDate(date: Date, format: 'YYYY年MM月DD日HH時mm分ss秒'): string;
+
+/**
+ * 日時を YYYY年M月D日H時m分 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY年M月D日H時m分
+ * @returns {string} 例: 2024年4月8日14時30分
+ */
+export function formatDate(date: Date, format: 'YYYY年M月D日H時m分'): string;
+
+/**
+ * 日時を YYYY年M月D日H時 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY年M月D日H時
+ * @returns {string} 例: 2024年4月8日14時
+ */
+export function formatDate(date: Date, format: 'YYYY年M月D日H時'): string;
+
+/**
+ * 日時を YYYY年M月D日HH時mm分ss秒 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY年M月D日HH時mm分ss秒
+ * @returns {string} 例: 2024年4月8日09時05分05秒
+ */
+export function formatDate(date: Date, format: 'YYYY年MM月DD日HH時mm分ss秒'): string;
+
+/**
+ * 日時を YYYY年M月D日H時m分 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY年M月D日H時m分
+ * @returns {string} 例: 2024年4月8日9時5分
+ */
+export function formatDate(date: Date, format: 'YYYY年M月D日H時m分'): string;
+
+/**
+ * 日時を YYYY年M月D日H時 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format YYYY年M月D日H時
+ * @returns {string} 例: 2024年4月8日9時
+ */
+export function formatDate(date: Date, format: 'YYYY年M月D日H時'): string;
+
+/**
+ * 日時を (元号)Y年M月D日H時m分 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format (元号)Y年M月D日H時m分
+ * @returns {string} 例: 令和6年4月8日9時30分
+ */
+export function formatDate(date: Date, format: '(元号)Y年M月D日H時m分'): string;
+
+/**
+ * 日時を (元号)Y年M月D日H時 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format (元号)Y年M月D日H時
+ * @returns {string} 例: 令和6年4月8日9時
+ */
+export function formatDate(date: Date, format: '(元号)Y年M月D日H時'): string;
+
+/**
+ * 日時を MM/DD HH:mm:ss 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format MM/DD HH:mm:ss
+ * @returns {string} 例: 04/08 09:05:45
+ */
+export function formatDate(date: Date, format: 'MM/DD HH:mm:ss'): string;
+
+/**
+ * 日時を MM/DD HH:mm 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format MM/DD HH:mm
+ * @returns {string} 例: 04/08 09:05
+ */
+export function formatDate(date: Date, format: 'MM/DD HH:mm'): string;
+
+/**
+ * 日時を M月D日HH時mm分ss秒 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format M月D日HH時mm分ss秒
+ * @returns {string} 例: 4月8日09時05分05秒
+ */
+export function formatDate(date: Date, format: 'MM月DD日HH時mm分ss秒'): string;
+
+/**
+ * 日時を M月D日H時m分 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format M月D日H時m分
+ * @returns {string} 例: 4月8日9時5分
+ */
+export function formatDate(date: Date, format: 'M月D日H時m分'): string;
+
+/**
+ * 日時を M月D日H時 形式の文字列に変換します
+ * @param date 変換対象の日時
+ * @param format M月D日H時
+ * @returns {string} 例: 4月8日9時
+ */
+export function formatDate(date: Date, format: 'M月D日H時'): string;
+
+/**
+ * 日時から特定の要素を抽出します
+ * @param date 変換対象の日時
+ * @param format 
+ * - 'YYYY': 年（4桁）
+ * - 'MM': 月（2桁）
+ * - 'M': 月
+ * - 'DD': 日（2桁）
+ * - 'D': 日
+ * - 'HH': 時（2桁）
+ * - 'H': 時
+ * - 'mm': 分（2桁）
+ * - 'm': 分
+ * - 'ss': 秒（2桁）
+ * - 's': 秒
+ * @returns {string} 指定された要素の文字列
+ */
+export function formatDate(date: Date, format: 'YYYY' | 'MM' | 'M' | 'DD' | 'D' | 'HH' | 'H' | 'mm' | 'm' | 'ss' | 's'): string;
+
+// 実装
+export function formatDate(date: Date, format: SupportedDateFormat): string {
+
+  switch (format) {
+    case 'YYYY-MM-DD':
+      return date.toLocaleDateString('en-CA', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      });
+
+    case 'YYYY/MM/DD':
+      return date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      });
+
+    case 'YYYY/MM/DD(ddd)':
+      return `${date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      })}(${date.toLocaleDateString('en-US', { weekday: 'short' })})`;
+
+    case 'YYYY/MM/DD(曜)':
+      return date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        weekday: 'short'
+      });
+
+    case 'YYYY年M月D日':
+      return date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      });
+
+    case 'YYYY年M月D日(曜)':
+      return date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'short'
+      });
+
+    case '(元号)Y年M月D日':
+      return date.toLocaleDateString('ja-JP-u-ca-japanese', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      });
+
+    case '(元号)Y年M月D日(曜)':
+      return date.toLocaleDateString('ja-JP-u-ca-japanese', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'short'
+      });
+
+    case '(GENGOU)Y.M.D':
+      return date.toLocaleDateString('ja-JP-u-ca-japanese', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'
+      }).replaceAll('/', '.');
+
+    case '(GENGOU)Y.MM.DD':
+      return date.toLocaleDateString('ja-JP-u-ca-japanese', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      }).replaceAll('/', '.');
+
+    case 'MM/DD':
+      return date.toLocaleDateString('ja-JP', {
+        month: '2-digit',
+        day: '2-digit',
+      });
+
+    case 'MM/DD(ddd)':
+      return `${date.toLocaleDateString('ja-JP', {
+        month: '2-digit',
+        day: '2-digit'
+      })}(${date.toLocaleDateString('en-US', { weekday: 'short' })})`;
+
+    case 'MM/DD(曜)':
+      return date.toLocaleDateString('ja-JP', {
+        month: '2-digit',
+        day: '2-digit',
+        weekday: 'short'
+      });
+
+    case 'M/D':
+      return date.toLocaleDateString('ja-JP', {
+        month: 'numeric',
+        day: 'numeric',
+      });
+
+    case 'M/D(ddd)':
+      return `${date.toLocaleDateString('ja-JP', {
+        month: 'numeric',
+        day: 'numeric',
+      })}(${date.toLocaleDateString('en-US', { weekday: 'short' })})`;
+
+    case 'M/D(曜)':
+      return date.toLocaleDateString('ja-JP', {
+        month: 'numeric',
+        day: 'numeric',
+        weekday: 'short'
+      });
+
+    case 'M月D日':
+      return date.toLocaleDateString('ja-JP', {
+        month: 'long',
+        day: 'numeric'
+      });
+
+    case 'M月D日(曜)':
+      return date.toLocaleDateString('ja-JP', {
+        month: 'long',
+        day: 'numeric',
+        weekday: 'short'
+      });
+
+    case 'HH:mm:ss.SSS':
+      return `${
+        date.toLocaleTimeString('ja-JP', {
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          hour12: false
+        })
+      }.${String(date.getMilliseconds()).padStart(3, '0')}`;
+
+    case 'HH:mm:ss':
+      return date.toLocaleTimeString('ja-JP', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+      });
+
+    case 'HH:mm':
+      return date.toLocaleTimeString('ja-JP', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+      });
+
+    case 'HH時mm分ss秒':
+      return `${
+        String(date.getHours()).padStart(2, '0')
+      }時${
+        String(date.getMinutes()).padStart(2, '0')
+      }分${
+        String(date.getSeconds()).padStart(2, '0')
+      }秒`;
+
+    case 'HH時mm分':
+      return `${
+        String(date.getHours()).padStart(2, '0')
+      }時${
+        String(date.getMinutes()).padStart(2, '0')
+      }分`;
+
+    case 'H時m分':
+      return `${date.getHours()}時${date.getMinutes()}分`;
+
+    case 'HH時':
+      return `${String(date.getHours()).padStart(2, '0')}時`;
+
+    case 'H時':
+      return `${date.getHours()}時`;
+
+    case 'ISO8601':
+    case 'YYYY-MM-DDTHH:mm:ss.SSSZ':
+      return date.toISOString();
+
+    case 'YYYY-MM-DD HH:mm:ss':
+      return `${date.toLocaleDateString('en-CA', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      })} ${date.toLocaleTimeString('ja-JP', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })}`;
+
+    case 'YYYY/MM/DD HH:mm:ss':
+      return date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      });
+
+    case 'YYYY/MM/DD HH:mm':
+      return date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+
+    case 'YYYY年MM月DD日HH時mm分ss秒':
+      return `${
+        date.getFullYear()
+      }年${
+        String(date.getMonth() + 1).padStart(2, '0')
+      }月${
+        String(date.getDate()).padStart(2, '0')
+      }日${
+        String(date.getHours()).padStart(2, '0')
+      }時${
+        String(date.getMinutes()).padStart(2, '0')
+      }分${
+        String(date.getSeconds()).padStart(2, '0')
+      }秒`;
+
+    case 'YYYY年M月D日H時m分':
+      return `${
+        date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+        })
+      }${date.getHours()}時${date.getMinutes()}分`;
+
+    case 'YYYY年M月D日H時':
+      return `${
+        date.toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+       })
+      }${date.getHours()}時`;
+
+    case '(元号)Y年M月D日H時m分':
+      return `${
+        date.toLocaleDateString('ja-JP-u-ca-japanese', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+      }${date.getHours()}時${date.getMinutes()}分`;
+
+    case '(元号)Y年M月D日H時':
+      return `${
+        date.toLocaleDateString('ja-JP-u-ca-japanese', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+      }${date.getHours()}時`;
+
+    case 'MM/DD HH:mm:ss':
+      return date.toLocaleDateString('ja-JP', {
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      });
+
+    case 'MM/DD HH:mm':
+      return date.toLocaleDateString('ja-JP', {
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+
+    case 'MM月DD日HH時mm分ss秒':
+      return `${
+        String(date.getMonth() + 1).padStart(2, '0')
+      }月${
+        String(date.getDate()).padStart(2, '0')
+      }日${
+        String(date.getHours()).padStart(2, '0')
+      }時${
+        String(date.getMinutes()).padStart(2, '0')
+      }分${
+        String(date.getSeconds()).padStart(2, '0')
+      }秒`;
+
+    case 'M月D日H時m分':
+      return `${
+        date.toLocaleDateString('ja-JP', {
+        month: 'long',
+        day: 'numeric'
+        })
+      }${date.getHours()}時${date.getMinutes()}分`;
+
+    case 'M月D日H時':
+      return `${
+        date.toLocaleDateString('ja-JP', {
+        month: 'long',
+        day: 'numeric'
+        })
+      }${date.getHours()}時`;
+
+    case 'YYYY':
+      return String(date.getFullYear());
+
+    case 'MM':
+      return String(date.getMonth() + 1).padStart(2, '0');
+
+    case 'M':
+      return String(date.getMonth() + 1);
+
+    case 'DD':
+      return String(date.getDate()).padStart(2, '0');
+
+    case 'D':
+      return String(date.getDate());
+
+    case 'HH':
+      return String(date.getHours()).padStart(2, '0');
+
+    case 'H':
+      return String(date.getHours());
+
+    case 'mm':
+      return String(date.getMinutes()).padStart(2, '0');
+
+    case 'm':
+      return String(date.getMinutes());
+
+    case 'ss':
+      return String(date.getSeconds()).padStart(2, '0');
+
+    case 's':
+      return String(date.getSeconds());
+
+    default:
+      throw new Error(`Unsupported format: ${format}`);
+  }
+}
